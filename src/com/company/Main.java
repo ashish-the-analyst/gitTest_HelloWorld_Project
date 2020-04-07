c2ada7fd2ef0ad95b725c07896fc68ef46715545
ashishbhandari <ashish.id@hotmail.com>
2020-04-07 00:02:26 -0500

    Changes in the project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world ! from github.com..");
-        System.out.print("Sandesh Lamsal");
-	
+        System.out.println("Sandesh Lamsal");
+
+
+
+        //4/6/2020 2:20 pm
+        int[] intArray={100, 200, 300};
+        for (int a: intArray){
+            System.out.println(a);
+        }
     }
 }
