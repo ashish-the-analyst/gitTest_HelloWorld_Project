4b2a7816d5d48568fe721e620019261f7ad0112d
ashishbhandari <ashish.id@hotmail.com>
2020-04-07 22:48:25 -0500

    comments added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("hello world ! from github.com..");
         System.out.println("Sandesh Lamsal");
 
 
         //4/6/2020 2:20 pm
-        int[] intArray={100, 200, 300};
-        for (int a: intArray){
+        int[] intArray = {100, 200, 300};
+        for (int a : intArray) {
             System.out.println(a);
         }
+
+            System.out.println("This is for repository purpose");
+        }
     }
-}
